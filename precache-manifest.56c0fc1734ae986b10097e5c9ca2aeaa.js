f9c16ca63f3cf82f0c45f83b5827f26298973e47
Terence Kong <Terence_Kong@epam.com>
2020-06-23 06:00:43 +0800

    Updates

---
+++ precache-manifest.56c0fc1734ae986b10097e5c9ca2aeaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb752f3b7c519468227b2221313eb58f",
+    "url": "/reddit_clone_client/index.html"
+  },
+  {
+    "revision": "47b29682a8801165655c",
+    "url": "/reddit_clone_client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e76c2485c22367239ae",
+    "url": "/reddit_clone_client/static/js/2.5cb9f055.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit_clone_client/static/js/2.5cb9f055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b29682a8801165655c",
+    "url": "/reddit_clone_client/static/js/main.7af5d699.chunk.js"
+  },
+  {
+    "revision": "89e718dd35ccc4512dd6",
+    "url": "/reddit_clone_client/static/js/runtime-main.61f4b9ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit_clone_client/static/media/logo.5d5d9eef.svg"
+  }
+]);
