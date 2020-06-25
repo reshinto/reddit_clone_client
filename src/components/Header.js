6fde33d6b63cc38ff6ff2074775581c2f4db2cbb
Terence Kong <Terence_Kong@epam.com>
2020-06-25 13:12:30 +0800

    modify navbar styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import "./Header.css";
 
 export default (props) => {
+  let isLoggedIn = useSelector((state) => state.userReducer.isLoggedIn);
   const title = useSelector((state) => state.headerReducer.title, shallowEqual);
   const subtitle = useSelector(
     (state) => state.headerReducer.subtitle,
@@ -12,8 +13,8 @@ export default (props) => {
 
   return (
     <>
-      <Navbar />
       <div className="header-container">
+        {isLoggedIn ? <Navbar /> : null}
         <img className="header-image" alt="headerImage" />
         <h1>{title}</h1>
         <p>{subtitle}</p>
